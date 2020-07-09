e11e3e5ba125e090af0c52c707f975066e832a2a
Lisandro <lichuort@gmail.com>
2020-07-09 18:39:20 -0300

    TTS y un poco más

--- Examen de manejo LectO/Pages/Pag_Preguntas.xaml.cs
+++ Examen de manejo LectO/Pages/Pag_Preguntas.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Speech.Synthesis;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -21,10 +22,43 @@ namespace Examen_de_manejo_LectO.Pages
     public partial class Pag_Preguntas : Page
     {
         Cuestionario cuestionario;
+        SpeechSynthesizer synth;
+
         public Pag_Preguntas()
         {
             InitializeComponent();
             cuestionario = new Cuestionario();
+            synth = new SpeechSynthesizer();
+        }
+
+        private void ttsPregunta_Click(object sender, RoutedEventArgs e)
+        {
+            synth.SpeakAsyncCancelAll();
+            synth.SpeakAsync(textBoxPregunta.Text);
+        }
+
+        private void ttsA_Click(object sender, RoutedEventArgs e)
+        {
+            synth.SpeakAsyncCancelAll();
+            synth.SpeakAsync("Opción A \n" + textBoxA.Text);
+        }
+
+        private void ttsB_Click(object sender, RoutedEventArgs e)
+        {
+            synth.SpeakAsyncCancelAll();
+            synth.SpeakAsync("Opción B \n" + textBoxB.Text);
+        }
+
+        private void ttsC_Click(object sender, RoutedEventArgs e)
+        {
+            synth.SpeakAsyncCancelAll();
+            synth.SpeakAsync("Opción C \n" + textBoxC.Text);
+        }
+
+        private void ttsGeneral_Click(object sender, RoutedEventArgs e)
+        {
+            synth.SpeakAsyncCancelAll();
+            synth.SpeakAsync(textBoxPregunta.Text + "\nOpción A \n" + textBoxA.Text + "\nOpción B \n" + textBoxB.Text + "\nOpción C \n" + textBoxC.Text);
         }
     }
 }
