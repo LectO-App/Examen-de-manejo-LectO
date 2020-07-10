28c0ac2d62a508d99f4ed3a3ee1d1080beb3165c
FaustoFang <51834130+FaustoFang@users.noreply.github.com>
2020-07-10 12:30:39 -0300

    Icono de la app y nombre de ventana cambiados. Botones borrosos arreglados.

--- Examen de manejo LectO/Properties/Resources.Designer.cs
+++ Examen de manejo LectO/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace Examen_de_manejo_LectO.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon logoletra {
+            get {
+                object obj = ResourceManager.GetObject("logoletra", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
